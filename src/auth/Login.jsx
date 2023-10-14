ad935e0ff349437082ca74e404f71c56098dd96d
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-10-14 07:29:32 +0530

    add login and register

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,3 +1,20 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container maxWidth="sm">
+      <Box>
+        <Typography variant="h3" style={{ marginBlock: "30px" }}>
+          Login
+        </Typography>
+        <form>
+          <TextField margin="normal" fullWidth label="Email" />
+          <TextField margin="normal" fullWidth label="Password" />
+          <Button style={{ marginTop: "20px" }} fullWidth variant="contained" type="submit" color="success" size="large">
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
 }
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,3 +1,30 @@
+import { Button, Container, Grid, TextField } from "@mui/material";
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <Container maxWidth="sm">
+      <h1>Register</h1>
+      <form>
+        <Grid container spacing={2}>
+          <Grid item sm={12}>
+            <TextField label="First Name" fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField label="Last Name" fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField label="Email" fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField label="Password" fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <Button type="submit" variant="contained" color="success" fullWidth size="large">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 }
